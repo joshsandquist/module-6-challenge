54cabaea69e1e682380396c965a6ff5f5db680fe
Josh Sandquist <joshdsand@gmail.com>
2023-03-02 11:55:01 -0700

    refactored the local storage to save each city individually rather than in an array

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,25 @@
 var APIKey = '73cac212d998043d18f6cc88473fb507';
 var submitButtonEl = document.querySelector('#submitBtn');
 var cityInputEl = document.querySelector('#cityInput');
-var cities = [];
 
 submitButtonEl.addEventListener('click', function() {
   var city = cityInputEl.value;
   var cityDataUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
-  cities.push(cityDataUrl);
-  localStorage.setItem('cities', JSON.stringify(cities));
+  localStorage.setItem(city, cityDataUrl);
 });
 
-var cities = JSON.parse(localStorage.getItem('cities'));
-if (cities) {
-  cities.forEach(function(cityDataUrl) {
-    fetch(cityDataUrl)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        
-      })
-  });
+for (var i = 0; i < localStorage.length; i++) {
+  var cityName = localStorage.key(i);
+  var cityDataUrl = localStorage.getItem(cityName);
+  fetch(cityDataUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var cityName = data.name;
+      var buttonEl = document.createElement('button');
+      buttonEl.textContent = cityName;
+      buttonEl.classList = ('btn')
+      document.querySelector('#searchbar').appendChild(buttonEl);
+    })
 }
